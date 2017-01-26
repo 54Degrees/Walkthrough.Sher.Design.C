a88f7911beea2f2fa855ba5b689525af9bb20b33
Ellen Corcoran-Walsh <54degreesdesign@gmail.com>
2017-01-26 13:46:28 +0000

    Walkthrough for Sher Design (Kitchen C)

---
+++ data.js
@@ -0,0 +1,182 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-sher-design-kitchen-c1",
+      "name": "Sher Design Kitchen C1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.38771735418585784,
+        "pitch": 0.2404004599699583,
+        "fov": 1.2341091574053615
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6305643109478076,
+          "pitch": 0.5850876618300251,
+          "rotation": 0,
+          "target": "2-sher-design-kitchen-c3"
+        },
+        {
+          "yaw": -0.8436573806500451,
+          "pitch": 0.31684801323123324,
+          "rotation": 0,
+          "target": "1-sher-design-kitchen-c2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.020809410130473438,
+          "pitch": 1.3994347240459977,
+          "title": "Created by 54 Degrees Design",
+          "text": "<div>109 Maydown Road</div><div>Dungannon</div><div>BT71 7LJ</div><div><br></div><div>07484 786050</div><div><br></div><div>info@54DegreesDesign.com</div><div><br></div><div>www.54DegreesDesign.com</div>"
+        },
+        {
+          "yaw": -3.1182612983868108,
+          "pitch": 1.4158459583135254,
+          "title": "Sher Design Kitchens",
+          "text": "<div>112 Farriter Road</div><div>Dungannon</div><div><br></div><div>07841 431933</div><div><br></div><div>info@sher-design.com</div><div><br></div><div>www.Sher-design.com</div>"
+        }
+      ]
+    },
+    {
+      "id": "1-sher-design-kitchen-c2",
+      "name": "Sher Design Kitchen C2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.1790049619561103,
+        "pitch": 0.11942271014747874,
+        "fov": 1.1855814804714877
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.8973571692161393,
+          "pitch": 0.7445001707886618,
+          "rotation": 0,
+          "target": "2-sher-design-kitchen-c3"
+        },
+        {
+          "yaw": 2.049424208936199,
+          "pitch": 0.4929750692207264,
+          "rotation": 0,
+          "target": "0-sher-design-kitchen-c1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.022160003466492384,
+          "pitch": 1.4035704665394526,
+          "title": "Created by 54 Degrees Design",
+          "text": "<div>109 Maydown Road</div><div>Dungannon</div><div>BT71 7LJ</div><div><br></div><div>07484 786050</div><div><br></div><div>info@54DegreesDesign.com</div><div><br></div><div>www.54DegreesDesign.com</div>"
+        },
+        {
+          "yaw": -3.1007558402534254,
+          "pitch": 1.403323128666834,
+          "title": "Sher Design Kitchens",
+          "text": "<div>112 Farriter Road</div><div>Dungannon</div><div><br></div><div>07841 431933</div><div><br></div><div>info@sher-design.com</div><div><br></div><div>www.Sher-design.com</div>"
+        }
+      ]
+    },
+    {
+      "id": "2-sher-design-kitchen-c3",
+      "name": "Sher Design Kitchen C3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 3.1013343393767947,
+        "pitch": 0.27015259795522617,
+        "fov": 1.3856488727416894
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.8824288147488346,
+          "pitch": 0.6040597930464813,
+          "rotation": 0,
+          "target": "0-sher-design-kitchen-c1"
+        },
+        {
+          "yaw": 1.9681587293195655,
+          "pitch": 0.7966131766377931,
+          "rotation": 0,
+          "target": "1-sher-design-kitchen-c2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -3.135986966490462,
+          "pitch": 1.3978975832981462,
+          "title": "Sher Design Kitchens",
+          "text": "<div>112 Farriter Road</div><div>Dungannon</div><div><br></div><div>07841 431933</div><div><br></div><div>info@sher-design.com</div><div><br></div><div>www.Sher-design.com</div>"
+        },
+        {
+          "yaw": -0.00268267711511605,
+          "pitch": 1.3902496970168219,
+          "title": "Created by 54 Degrees Design",
+          "text": "<div>109 Maydown Road</div><div>Dungannon</div><div>BT71 7LJ</div><div><br></div><div>07484 786050</div><div><br></div><div>info@54DegreesDesign.com</div><div><br></div><div>www.54DegreesDesign.com</div>"
+        }
+      ]
+    }
+  ],
+  "name": "Sher Design Kitchens C",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
